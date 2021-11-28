3c193c04a61ee65e6b5b4982b3b789deee4b1823
Xavier Loera Flores <x.loera.tech@gmail.com>
2021-11-27 23:10:42 -0800

    added basic routes

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,30 @@ router.get('/', (req,res,next)=>{
 })
 
 
+router.post('/transactions', (req, res, next) =>{
+    res.send({ 
+        message:"POST:/transactions"
+    })
+})
+
+router.post('/spend', (req, res, next) =>{
+    res.send({ 
+        message:"POST:/spend"
+    })
+})
+
+router.get('/balances', (req, res, next) =>{
+    res.send({ 
+        message:"GET:/balances"
+    })
+})
+
+
+
+
+
+
+
 router.use((error, req, res, next)=>{
     console.error(error)
     res.status(500)
